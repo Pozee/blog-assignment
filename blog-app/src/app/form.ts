40235921500eda0fefa66975f4c31f3d6f441caa
Arthur Oresjö <arthur_jul@hotmail.com>
2020-03-18 15:17:18 +0100

    fult formulär med basic validering

--- blog-app/src/app/form.ts
+++ blog-app/src/app/form.ts
@@ -3,8 +3,8 @@ export class Form {
     constructor(
         public title: string,
         public content: string,
-        public readtime: number,
-        public author: string
+        public readtime: string,
+        public author?: string
 
     
 
